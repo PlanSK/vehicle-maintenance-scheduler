50c5fed41f8a50ccc9531b01d94ec42996b9f005
PlanSK <skapkanshchikov@gmail.com>
2024-06-05 21:57:02 +0300

    Change design for app. Add progress bar and counter for every event.

--- maintenance/services/maintenance.py
+++ maintenance/services/maintenance.py
@@ -24,6 +24,8 @@ class PlanedWork:
     planed_date: datetime.date
     date_delta: datetime.timedelta
     last_event_date: datetime.date
+    remaining_procentage: int
+    current_event_counter: int
 
 
 def get_maintenance_limits(vin_code: str) -> list[PlanedWork]:
@@ -34,12 +36,18 @@ def get_maintenance_limits(vin_code: str) -> list[PlanedWork]:
     for current_work in Work.objects.filter(
             vehicle__vin_code=vin_code,
             work_type=Work.WorkType.MAINTENANCE):
-        last_event: Event = vehicle_events.filter(
-            work=current_work).order_by('-work_date').first()
+        current_event_list: Event = vehicle_events.filter(
+            work=current_work).order_by('-work_date')
+        current_event_counter = current_event_list.count()
+        last_event = current_event_list.first()
         if (not last_event or not current_work.interval_km
                 and not current_work.interval_month):
             continue
         limit_mileage = last_event.mileage + current_work.interval_km
+        mileage_delta=limit_mileage - current_vehicle.vehicle_mileage
+        mileage_remaining_procentage = 100 - round(
+            (mileage_delta / current_work.interval_km) * 100
+        )
         limit_date = last_event.work_date + relativedelta(
             months=current_work.interval_month)
         if current_vehicle.vehicle_mileage >= limit_mileage:
@@ -53,10 +61,12 @@ def get_maintenance_limits(vin_code: str) -> list[PlanedWork]:
                 work=current_work,
                 trigger=work_triger,
                 planed_mileage=limit_mileage,
-                mileage_delta=limit_mileage - current_vehicle.vehicle_mileage,
+                mileage_delta=mileage_delta,
                 planed_date=limit_date,
                 date_delta=timezone.now().date() - limit_date,
-                last_event_date=last_event.work_date
+                last_event_date=last_event.work_date,
+                remaining_procentage=mileage_remaining_procentage,
+                current_event_counter = current_event_counter
             )
         )
 
