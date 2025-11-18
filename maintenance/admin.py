61435101df9992f79a7a5600d4ffdcfaa5343d19
PlanSK <skapkanshchikov@gmail.com>
2025-11-19 01:33:39 +0300

    Event model registration in admin.py

--- maintenance/admin.py
+++ maintenance/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from maintenance.models import Vehicle, WorkPattern, Work
+from maintenance.models import Vehicle, WorkPattern, Work, Event
 
 
 admin.site.register(Vehicle)
 admin.site.register(WorkPattern)
 admin.site.register(Work)
+admin.site.register(Event)
